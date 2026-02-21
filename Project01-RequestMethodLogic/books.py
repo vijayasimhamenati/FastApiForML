de911a8f6f35f1e632d936b6fec07b58efb2b101
= <vijayasimhamenati@gmail.com>
2026-02-21 11:32:29 +0530

    c02 return a real book of list

--- Project01-RequestMethodLogic/books.py
+++ Project01-RequestMethodLogic/books.py
@@ -2,9 +2,43 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+books = [
+  {
+    "id": 1,
+    "title": "To Kill a Mockingbird",
+    "author": "Harper Lee",
+    "year": 1960
+  },
+  {
+    "id": 2,
+    "title": "1984",
+    "author": "George Orwell",
+    "year": 1949
+  },
+  {
+    "id": 3,
+    "title": "The Great Gatsby",
+    "author": "F. Scott Fitzgerald",
+    "year": 1925
+  },
+  {
+    "id": 4,
+    "title": "The Catcher in the Rye",
+    "author": "J.D. Salinger",
+    "year": 1951
+  },
+  {
+    "id": 5,
+    "title": "Pride and Prejudice",
+    "author": "Jane Austen",
+    "year": 1813
+  }
+]
+
+
 @app.get("/books")
-async def get_books():
-    return {"message":"All books"}
+async def get_all_books():
+    return books
 
 @app.get("/")
 async def get_home():
