c3221003cdd4525410459339d0b6a8afb80cc5ee
= <vijayasimhamenati@gmail.com>
2026-04-04 22:19:31 +0530

    project 1 done

--- Project01-RequestMethodLogic/books.py
+++ Project01-RequestMethodLogic/books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -60,7 +60,7 @@ async def get_all_books():
     return books 
 
 @app.get("/books/{book_id}")
-async def get_book(book_id: int):
+async def get_book(book_id: str):
     for book in books:
         if book['id'] == book_id :
             return book
@@ -78,6 +78,50 @@ async def get_books_by_category(category: str) :
     
 
     return books_to_return
+
+@app.get("/book/")
+async def get_books_by_author(author_name: str):
+    books_to_return =[]
+    for book in books:        
+        if book.get("author").lower() == author_name.lower():
+            books_to_return.append(book)
+
+    if len(books_to_return)!=0:
+        return books_to_return
+    else:
+        return{"message" : "No books registered with given author"}
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    books.append(new_book)
+    return {"message" : "book created successfully",
+            "book" : new_book}
+
+@app.put("/books/update_book/{book_id}")
+async def update_book(book_id: str, updated_book=Body()):
+    # books[book_id] = updated_book
+    book_index = None
+    for book in books:
+        if book['id'] == book_id :
+            book_index = books.index(book)
+            break
+    if book_index is not None:
+        books[book_index] = updated_book
+
+    return {"message" : "book updated successfully"},
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title : str):
+    book_to_delete = None
+    for i in range(len(books)):
+        
+        books[i].get("title").lower() == book_title.lower()
+        book_to_delete = i
+        break
+    books.pop(book_to_delete)
+
+    return {"message" : "Book deleted successfully"}
 
 # uvicorn Project01-RequestMethodLogic.books:app --reload
 
